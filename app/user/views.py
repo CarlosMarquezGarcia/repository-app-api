86bd941a75e4665a3653a9faa02ab5acb3807b8b
carlos marquez <marquezga.carlos@gmail.com>
2022-11-26 18:15:21 -0600

    Fixed linting

--- app/user/views.py
+++ app/user/views.py
@@ -3,7 +3,6 @@ Views for the User API.
 """
 from rest_framework import generics, authentication, permissions
 
-from .serializers import UserSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
